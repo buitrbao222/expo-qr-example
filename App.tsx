f83ebb6ae0be9e4a05ec8f577ca679e24941d284
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-12-06 10:55:42 +0700

    create global styles object

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[styles.flex1]}>
       <StatusBar style="auto" />
+
+      <View></View>
+
+      <View style={[styles.bgRed]}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
